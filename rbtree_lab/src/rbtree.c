aeffdd65b5743ebfbd2d9d0b9609fc75d4e3db03
crucial-sub <jssub940@gmail.com>
2025-08-15 13:45:40 +0900

    BST 삽입 구현

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -5,6 +5,8 @@
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
+  p->root = NULL;
+  
   return p;
 }
 
@@ -15,7 +17,42 @@ void delete_rbtree(rbtree *t) {
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
-  return t->root;
+  if (!t) return NULL;
+
+  // node 초기 설정
+  node_t *node = (node_t *)calloc(1, sizeof(node_t));
+  node->parent = node->left = node->right = NULL;
+  node->color = (!(t->root) ? RBTREE_BLACK : RBTREE_RED);
+  node->key = key;
+
+  // 트리가 비어있으면 바로 루트로 삼고 함수 종료
+  if (!(t->root)) 
+  {
+    t->root = node;
+    return node;
+  }
+
+  // BST 삽입 먼저 구현
+  node_t *parent = NULL;
+  node_t *tmp = t->root;
+  while (tmp) {
+    parent = tmp;
+    tmp = (key < tmp->key) ? tmp->left : tmp->right;
+  }
+
+  node->parent = parent;
+  if (key < parent->key) {
+    parent->left = node;
+  } else {
+    parent->right = node;
+  }
+
+  if (node->parent->color == RBTREE_RED) 
+  {
+    insert_fixup(t, node);
+  }
+
+  return node;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
